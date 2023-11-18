3904ae17ed9e1452ffc55dde75159ed10dcef4f1
yermtat <yerm.tat@gmail.com>
2023-11-18 23:00:39 +0400

    send help

---
+++ СargoProject/Models/DeclarationModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace СargoProject.Models;
+
+internal class DeclarationModel : IData
+{
+    public string Warehouse { get; set; }
+    public string SiteName { get; set; }
+}
